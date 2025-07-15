a203f50c028b5db8012e874b16a190a2fd2f1c0a
xiaogao <15290171627@163.com>
2025-07-15 21:30:04 +0800

    父子组件通信、State

--- src/views/comment.tsx
+++ src/views/comment.tsx
@@ -6,8 +6,8 @@ class UserInfo extends Component {
     return (
       <div>
         <div className='user-info'>
-            <img className='user-avatar' src={this.props.avatar} alt="User Avatar" />
-        <span>{this.props.name}</span>
+          <img className='user-avatar' src={this.props.avatar} alt="User Avatar" />
+          <span>{this.props.name}</span>
         </div>
         <p>Email: {this.props.email}</p>
       </div>
@@ -15,24 +15,68 @@ class UserInfo extends Component {
   }
 }
 class CommentCont extends Component {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      publishTime: '2025/07/15 21:30:00'
+    }
+  }
+  handleUpdateDate = () => {
+    this.props.updateDate(new Date().toLocaleString())
+  }
   render() {
     return (
       <div>
         <h3>Comment Content</h3>
         <p>This is a comment content area where users can write their comments.</p>
-        <p>发布时间：{new Date().toLocaleString()}</p>
+        <p>发布时间：{this.state.publishTime}</p>
+        <button onClick={this.handleUpdateDate}>更新</button>
       </div>
     )
   }
 }
 
 export default class Comment extends Component {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      updateTime: '2025/07/15 21:13:00'
+    }
+  }
+  updeTime = new Date().toLocaleString()
+  // 第一种
+  // updateDate = (val: any) => {
+  //   this.setState({
+  //     updateTime: val
+  //   })
+  // }
+
+  // 第二种
+  // updateDate(e){
+  // console.log(e);
+  //   this.setState({
+  //     updateTime: new Date().toLocaleString()
+  //   })
+  // }
+
+  // 第三种
+  updateDate(){
+    this.setState({
+      updateTime: new Date().toLocaleString()
+    })
+  }
   render() {
+    console.log('render');
     return (
       <div>
         <div>
-            <UserInfo {...this.props.UserInfo} />
-            <CommentCont />
+          <UserInfo {...this.props.UserInfo} />
+          <p>更新时间：{this.state.updateTime}</p>
+          {/* 第一种 */}
+          {/* <CommentCont updateDate={this.updateDate} /> */}
+          {/* 第二种：可传递参数 */}
+          {/* <CommentCont updateDate={(e)=>{this.updateDate(e)}} /> */}
+          <CommentCont updateDate={this.updateDate.bind(this)} />
         </div>
       </div>
     )
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -2,17 +2,13 @@ import React, { Component } from 'react'
 import Comment from "./comment";
 import logoUrl from '@/assets/logo.jpg'
 
-interface UserInfo {
-    name: string;
-    email: string;
-    avatar: string;
-}
 export default class MyApp extends Component {
     UserInfo = {
         name: 'XiaoGao',
         email: 'xiaogao@example.com',
         avatar: logoUrl
     };
+  
     render() {
         return (
             <div>
