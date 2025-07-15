36ca545d5abeac297cc3489c31fd8746c1680b94
xiaogao <15290171627@163.com>
2025-07-15 21:29:35 +0800

    pnpm

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,9 @@ import MyApp from "./views/index.tsx";
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  state = {
-    isDialogOpen: false
-  }
-
-  handleDialog = () => {
-    this.setState({ isDialogOpen: !this.state.isDialogOpen })
-  }
-
   render() {
     return (
       <div>
-        {/* <XgButton title="按钮1" />
-        <XgButton title="按钮2" />
-        <button onClick={this.handleDialog}>弹框</button>
-        <XgDialog content="这是一个测试对话框" open={this.state.isDialogOpen} /> */}
         <MyApp />
       </div>
     )
