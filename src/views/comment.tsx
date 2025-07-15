8c2ed2fcec1d355d3921d6f6126da2657cc5c332
xiaogao <15290171627@163.com>
2025-07-15 21:59:05 +0800

    setState

--- src/views/comment.tsx
+++ src/views/comment.tsx
@@ -24,6 +24,9 @@ class CommentCont extends Component {
   handleUpdateDate = () => {
     this.props.updateDate(new Date().toLocaleString())
   }
+  add(){
+    this.props.add()
+  }
   render() {
     return (
       <div>
@@ -31,6 +34,7 @@ class CommentCont extends Component {
         <p>This is a comment content area where users can write their comments.</p>
         <p>发布时间：{this.state.publishTime}</p>
         <button onClick={this.handleUpdateDate}>更新</button>
+        <button onClick={this.add.bind(this)}>++</button>
       </div>
     )
   }
@@ -40,7 +44,8 @@ export default class Comment extends Component {
   constructor(props: any) {
     super(props)
     this.state = {
-      updateTime: '2025/07/15 21:13:00'
+      updateTime: '2025/07/15 21:13:00',
+      count:0
     }
   }
   updeTime = new Date().toLocaleString()
@@ -65,18 +70,38 @@ export default class Comment extends Component {
       updateTime: new Date().toLocaleString()
     })
   }
+
+  add(){
+    // setState:异步操作
+
+    // // 1、对象形式
+    // this.setState({
+    //   count:this.state.count+1
+    // })
+
+    // 2、函数形式,prveProps:回调函数，在状态改变后执行
+    this.setState((prevState: any,prveProps: any) => {
+      return {
+        count: prevState.count + 1
+      }
+    },()=>{
+      console.log(this.state.count);
+    })
+    
+    
+  }
   render() {
     console.log('render');
     return (
       <div>
         <div>
           <UserInfo {...this.props.UserInfo} />
-          <p>更新时间：{this.state.updateTime}</p>
+          <p><span>{this.state.count}</span>更新时间：{this.state.updateTime}</p>
           {/* 第一种 */}
           {/* <CommentCont updateDate={this.updateDate} /> */}
           {/* 第二种：可传递参数 */}
           {/* <CommentCont updateDate={(e)=>{this.updateDate(e)}} /> */}
-          <CommentCont updateDate={this.updateDate.bind(this)} />
+          <CommentCont updateDate={this.updateDate.bind(this)} add={this.add.bind(this)}/>
         </div>
       </div>
     )
